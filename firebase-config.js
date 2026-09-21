7a920323eef37113e35a5c37b19ccc85b5bfacd8
William Alvarez <profewilliam2019@gmail.com>
2026-09-21 15:50:02 -0500

    Conectar el proyecto al Firebase real del profesor (nuvora-tienda-fundadores)

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-// Configuración del proyecto Firebase "tienda-ropa-fundadores"
+// Configuración del proyecto Firebase "nuvora-tienda-fundadores"
 // Generada automáticamente al crear la app web en Firebase Console.
 const firebaseConfig = {
-  apiKey: "AIzaSyD-kQcs_uudepqtu0TcgaMABOOatU8iWZE",
-  authDomain: "tienda-ropa-fundadores.firebaseapp.com",
-  projectId: "tienda-ropa-fundadores",
-  storageBucket: "tienda-ropa-fundadores.firebasestorage.app",
-  messagingSenderId: "825155029110",
-  appId: "1:825155029110:web:7465a77587bfef7204b757"
+  apiKey: "AIzaSyA8SVv5a-USduURTKEOU3hsDil0w-qcg8M",
+  authDomain: "nuvora-tienda-fundadores.firebaseapp.com",
+  projectId: "nuvora-tienda-fundadores",
+  storageBucket: "nuvora-tienda-fundadores.firebasestorage.app",
+  messagingSenderId: "874972124504",
+  appId: "1:874972124504:web:8db2255e3767c0e20f012d"
 };
 
 firebase.initializeApp(firebaseConfig);
